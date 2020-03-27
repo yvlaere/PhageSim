e5f2dc8aade73e63edc877e0edde9c913d6777b5
michielstock <michielfmstock@gmail.com>
2020-03-27 18:56:46 +0100

    :wrench: update scripts

--- scripts/phagesspread.jl
+++ scripts/phagesspread.jl
@@ -1,6 +1,6 @@
 #=
 Created on Friday 27 December 2019
-Last update: Friday 21 Feb 2020
+Last update: Friday 27 March 2020
 
 @author: Michiel Stock
 michielfmstock@gmail.com
@@ -23,7 +23,7 @@ nsteps = 100
 anim = @animate for t=1:nsteps+1
     heatmap(grid, title="step $t\n $(sum(grid)) particles",
                 clims=(0, 10), color=:viridis, framestyle=:none)
-    updatephages!(grid, phagerules)
+    step_phages!(grid, phagerules)
 end
 
 gif(anim, plotsdir("phagespread.gif"), fps = 10)
