a21ad140ec2b5c5da65addd6a7e6d29e00958686
michielstock <michielfmstock@gmail.com>
2020-04-09 14:25:34 +0200

    :tada: function counting latent phages

--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,6 @@
 #=
 Created on Wednesday 26 March 2020
-Last update: Friday 30 March 2020
+Last update: Thursday 9 April 2020
 
 @author: Michiel Stock
 michielfmstock@gmail.com
@@ -8,7 +8,7 @@ michielfmstock@gmail.com
 General utilies for modelling.
 =#
 
-export insimplex, species_counts
+export insimplex, species_counts, species_counts_lat
 
 """
     insimplex(x::AbstractFloat...)
@@ -29,4 +29,23 @@ function species_counts(bactgrid::BactGrid, phagegrids; nspecies::Int)
     bactcount = [nbacteria(bactgrid, sp) for sp in 1:nspecies]
     phagecount = sum.(phagegrids)
     return bactcount, phagecount
+end
+
+"""
+    species_counts_lat(bactgrid::BactGrid, phagegrids; nspecies::Int)
+
+Returns a tuple with a list counting all the bacteria of each species, a
+list counting all phages of each species and a matrix containing the number
+latent phages per bacterium species.
+"""
+function species_counts_lat(bactgrid::BactGrid, phagegrids; nspecies::Int)
+    # first count the bacteria of each species
+    nphages = length(phagegrids)
+    bactcount = [nbacteria(bactgrid, sp) for sp in 1:nspecies]
+    phagecount = sum.(phagegrids)
+    bacteria = filter(isbacterium, bactgrid)
+    prophagescount = [count(
+            b -> species(b) == i && haslatent(b) && prophage(b)==j, bacteria)
+            for i in 1:nspecies, j in 1:nphages]
+    return bactcount, phagecount, prophagescount
 end
